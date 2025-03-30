bbcc71560cf2d2d158e3188910c44125db040d1a
bangddong <gusehdqkd@naver.com>
2025-03-30 23:42:43 +0900

    feat : 순수 JPA에서 Data-JPA

--- src/main/java/stduy/datajpa/repository/MemberRepository.java
+++ src/main/java/stduy/datajpa/repository/MemberRepository.java
@@ -1,8 +1,11 @@
 package stduy.datajpa.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import stduy.datajpa.entity.Member;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+	List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
 }
